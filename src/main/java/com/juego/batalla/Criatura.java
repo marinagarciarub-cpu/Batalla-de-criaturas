50957499cc5f06fd268d3fe3ef11dc93dc778625
Marina Garcia <marina.garcia@qaracter.com>
2025-12-22 11:13:44 +0000

    Clase criatura metodos basicos

--- src/main/java/com/juego/batalla/Criatura.java
+++ src/main/java/com/juego/batalla/Criatura.java
@@ -1,4 +1,30 @@
 package com.juego.batalla;
 
-public class Criatura {
+public abstract class Criatura {
+    protected String nombre;
+    protected int salud;
+    protected int energia;
+
+    public Criatura(String nombre, int salud, int energia) {
+        this.nombre = nombre;
+        this.salud = salud;
+        this.energia = energia;
+    }
+
+    // Método que obliga a los hijos a implementar su propio ataque
+    public abstract void atacar(Criatura objetivo) throws EnergiaInsuficienteException, PersonajeDerrotadoException;
+
+    public void recibirDaño(int cantidad) {
+        this.salud -= cantidad;
+        if (this.salud < 0) this.salud = 0;
+        System.out.println(this.nombre + " ha recibido " + cantidad + " de daño. Salud: " + this.salud);
+    }
+
+    public boolean estaVivo() {
+        return this.salud > 0;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
 }
